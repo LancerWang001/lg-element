ca5abbb645baa4baba97ecfaeaf4b26d4ac76bba
LancerWang001 <wangandong001@gmail.com>
2022-04-01 20:38:25 +0800

    feat: add new component

---
+++ packages/link/src/link.vue
@@ -0,0 +1,36 @@
+<template>
+	<a
+		:href="disabled ? null : href"
+		:class="{ disabled, 'no-underline': !underline }"
+	>
+		<slot />
+	</a>
+</template>
+
+<script>
+export default {
+	name: 'LgLink',
+  props: {
+    href: {
+			type: String,
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		underline: {
+			type: Boolean,
+			default: false
+		}
+  }
+}
+</script>
+
+<style scoped>
+.disabled {
+	cursor: pointer;
+}
+.no-underline {
+	text-decoration: none;
+}
+</style>
