45ffeb1ffdce3c75f435ee82715bcb81325779d0
LancerWang001 <wangandong001@gmail.com>
2022-04-01 08:45:20 +0800

    lerna init

---
+++ packages/button/src/button.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<button v-bind="$attrs" @click="handleClick"><slot /></button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LgButton',
+	inheritAttrs: false,
+	methods: {
+		handleClick (evt) {
+			this.$emit('click', evt)
+			evt.preventDefault()
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ packages/steps/src/Steps.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="lg-steps">
+		<div class="lg-steps-line"></div>
+		<div
+			class="lg-step"
+			v-for="index of count"
+			:key="index"
+			:style="{ color: active >= index ? activeColor : defaultColor }"
+		>
+			{{ index }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LgSteps',
+	props: {
+		count: {
+			type: Number,
+			default: 3
+		},
+		active: {
+			type: Number,
+			default: 0
+		},
+		activeColor: {
+			type:  String,
+			default: 'red'
+		},
+		defaultColor: {
+			type: String,
+			default: 'green'
+		}
+	}
+}
+</script>
+
+<style scoped>
+@import url(./steps.css);
+</style>
